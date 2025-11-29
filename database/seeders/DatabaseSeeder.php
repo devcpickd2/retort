4940ca13e32dc9a6c2a1e20a9d2e8073ec1415e7
indralesmana100792 <indralesmana.mmb@gmail.com>
2025-11-29 10:59:35 +0700

    bug fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            RoleSeeder::class,
+            RolePermissionSeeder::class,
             UsersSeeder::class,
         ]);
     }
