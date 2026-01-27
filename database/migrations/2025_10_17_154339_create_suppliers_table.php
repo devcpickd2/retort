98fd75b05318b2e7b69fdafe77b4923f9446b66a
farhanizz21 <farhanizzuddin.12@gmail.com>
2026-01-27 13:43:43 +0700

    master supplier fix migration

--- database/migrations/2025_10_17_154339_create_suppliers_table.php
+++ database/migrations/2025_10_17_154339_create_suppliers_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('nama_supplier');
             $table->string('jenis_barang');
             $table->string('plant');
+            $table->string('alamat');
             $table->timestamps();
         });
     }
